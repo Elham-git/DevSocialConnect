4812e0bb3b9d7d073746b6288e8151529a5b9d60
Elham Ahmad~ <ahmedelham494@gmail.com>
2022-05-26 13:46:30 +0530

    Adding posts ,single post display done

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -6,6 +6,9 @@ import {
   UPDATE_LIKES,
   DELETE_POST,
   ADD_POST,
+  GET_POST,
+  // ADD_COMMENT,
+  // REMOVE_COMMENT
 } from "./types";
 
 // Get posts
@@ -76,6 +79,23 @@ export const addPost = (formData) => async (dispatch) => {
     });
 
     dispatch(setAlert("Post Created", "success"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Get post
+export const getPost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`);
+
+    dispatch({
+      type: GET_POST,
+      payload: res.data,
+    });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
